7e950609fd9c2e4825dd0737874d82a3f2b2895d
Philippe Recto <precto1285@gmal.com>
2019-03-20 07:00:46 -0700

    new test for redux

--- redux-test/src/App.js
+++ redux-test/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
 
@@ -7,11 +10,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
